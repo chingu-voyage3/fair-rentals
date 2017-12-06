68d73f2a840be38c4f234120e17a868067dd42a3
RL Brown <rlbrown72@gmail.com>
2017-12-06 12:01:54 -0500

    added front-end using create-react-app

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # bears-13
 
-A work in progress, coming soon...
+work-in-progress notes:
+
+* The server is written in Node, with Express.
+* The front-end was bootstrapped with Create-React-App.
+* The server serves the C-R-A `/build` folder as its root route, while also
+  handling data requests. (at the moment, that's just `/test-route`);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import path from 'path';
 
 const port = process.env.PORT || 3333;
 const app = express();
 
+/**
+ * A TEST 'get' ROUTE
+ */
 app.get('/test-route', (req, res) => {
   res.json({ message: '/test-route is responding' });
 });
 
+/* 
+* STATIC ROUTE TO SERVE FRONT-END REACT APP
+* 
+* (N.B.: you can make this line shorter, but `path.join` and `__dirname`
+* ensure that it will work on Windows, Linux, OSX)
+*/
+app.use(
+  express.static(path.join(__dirname, '../client/bears13-frontend/build'))
+);
+
+/**
+ * START SERVER
+ */
 app.listen(port, () => console.log(`server listening on ${port}`));
