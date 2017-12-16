67b93e0125668b887cbdcea286e902b0731b9a57
RL Brown <rlbrown72@gmail.com>
2017-12-16 17:30:45 -0500

    FourOhFour gag added

--- client/src/utils/FourOhFour.js
+++ client/src/utils/FourOhFour.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import { BigDiv } from '../utils/shared-styles';
+import Pic404 from '../utils/pic404.jpg';
+
 class FourOhFour extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
   render() {
-    return <div>404! NOT FOUND.</div>;
+    return (
+      <BigDiv>
+        <p>You found it. The 404.</p>
+        <img style={{ height: '20em', borderRadius: '10%' }} src={Pic404} alt="power equipment with the number 404" />
+      </BigDiv>
+    );
   }
 }
 
