74a6a95b4214e2f0532e3f4d7039683052432ee2
Spencer Brimley <sjbrimley26@live.com>
2017-12-27 09:36:07 -0700

    Fixed an error log
    It was popping up even when the function ran successfully. I also reformatted the query/mutation fields so we can change them easier later

--- src/types.js
+++ src/types.js
@@ -59,260 +59,276 @@ function getProjection (fieldASTs) {
   }, {});
 };
 
+const queryUserField = {
+  type: userType,
+  args: {
+    id: {
+      name: 'id',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
+    }
+  },
+  resolve: async function(_, {id}, source, fieldASTs) {
+    let found_user;
+    var projections = getProjection(fieldASTs);
+    let finder = async function (id) {
+      try {
+        await mongoose.connect(mongo_url, {
+          useMongoClient: true
+        });
+        await USER.findOne({
+          'id': id
+        }, projections)
+        .then((result) => {
+          found_user = result;
+        });
+      }
+      catch (err) {
+        console.log(err);
+      }
+    };
+    await finder(id);
+    return found_user;
+  }
+};
+
+const queryLocationField = {
+  type: locationType,
+  args: {
+    id: {
+      name: 'id',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
+    }
+  },
+  resolve: async function(_, {id}, source, fieldASTs) {
+    let found_location;
+    var projections = getProjection(fieldASTs);
+    let finder = async function (id) {
+      try {
+        await mongoose.connect(mongo_url, {
+          useMongoClient: true
+        });
+        await LOCATION.findOne({
+          'id': id
+        }, projections)
+        .then((result) => {
+          found_location = result;
+        });
+      }
+      catch (err) {
+        console.log(err);
+      }
+    };
+    await finder(id)
+    .then(()=>{
+      if (found_location.geo) {
+        found_location.geo = JSON.stringify(found_location.geo);
+      }
+    });
+    return found_location;
+  }
+};
+
+const queryReviewField = {
+  type: reviewType,
+  args: {
+    id: {
+      name: 'id',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
+    }
+  },
+  resolve: async function(_, {id}, source, fieldASTs) {
+    let found_review;
+    var projections = getProjection(fieldASTs);
+    let finder = async function (id) {
+      try {
+        await mongoose.connect(mongo_url, {
+          useMongoClient: true
+        });
+        await REVIEW.findOne({
+          'id': id
+        }, projections)
+        .then((result) => {
+          found_review = result;
+        });
+      }
+      catch (err) {
+        console.log(err);
+      }
+    };
+    await finder(id);
+    return found_review;
+  }
+};
+
 const query = new graphql.GraphQLObjectType({
   name: 'Query',
   fields: {
-    user: {
-      type: userType,
-      args: {
-        id: {
-          name: 'id',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        }
-      },
-      resolve: async function(_, {id}, source, fieldASTs) {
-        let found_user;
-        var projections = getProjection(fieldASTs);
-        let finder = async function (id) {
-          try {
-            await mongoose.connect(mongo_url, {
-              useMongoClient: true
-            });
-            await USER.findOne({
-              'id': id
-            }, projections)
-            .then((result) => {
-              found_user = result;
-            });
-          }
-          catch (err) {
-            console.log(err);
-          }
-        };
-        await finder(id);
-        return found_user;
-      }
-    },
-    location: {
-      type: locationType,
-      args: {
-        id: {
-          name: 'id',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        }
-      },
-      resolve: async function(_, {id}, source, fieldASTs) {
-        let found_location;
-        var projections = getProjection(fieldASTs);
-        let finder = async function (id) {
-          try {
-            await mongoose.connect(mongo_url, {
-              useMongoClient: true
-            });
-            await LOCATION.findOne({
-              'id': id
-            }, projections)
-            .then((result) => {
-              found_location = result;
-            });
-          }
-          catch (err) {
-            console.log(err);
-          }
-        };
-        await finder(id)
-        .then(()=>{
-          if (found_location.geo) {
-            found_location.geo = JSON.stringify(found_location.geo);
-          }
-        });
-        return found_location;
-      }
-    },
-    review: {
-      type: reviewType,
-      args: {
-        id: {
-          name: 'id',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        }
-      },
-      resolve: async function(_, {id}, source, fieldASTs) {
-        let found_review;
-        var projections = getProjection(fieldASTs);
-        let finder = async function (id) {
-          try {
-            await mongoose.connect(mongo_url, {
-              useMongoClient: true
-            });
-            await REVIEW.findOne({
-              'id': id
-            }, projections)
-            .then((result) => {
-              found_review = result;
-            });
-          }
-          catch (err) {
-            console.log(err);
-          }
-        };
-        await finder(id);
-        return found_review;
-      }
-    }
+    user: queryUserField,
+    location: queryLocationField,
+    review: queryReviewField
   }
 });
+
+const addReviewerField = {
+  type: userType,
+  args: {
+    id: {
+      name: 'id',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
+    },
+    name: {
+      name: 'user name',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+    },
+    avatar: {
+      name: 'avatar url',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+    }
+  },
+  resolve: async function(_, {id, name, avatar}) {
+    await mongoose.connect(mongo_url, {
+      useMongoClient: true
+    });
+    let new_user = new USER({
+      id: id,
+      name: name,
+      avatar: avatar,
+      reviews: [],
+      registered: Date()
+    });
+    return new_user.save((err) => {
+      if (err) {
+        return console.log('Unable to add new reviewer ', err);
+      }
+    });
+  }
+};
+
+const addLocationField = {
+  type: locationType,
+  args: {
+    id: {
+      name: 'id',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
+    },
+    name: {
+      name: 'user name',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+    },
+    avatar: {
+      name: 'avatar url',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+    },
+    geo: {
+      name: 'Geo coordinates',
+      type: new graphql.GraphQLNonNull(geoType),
+    }
+  },
+  resolve: async function(_, {id, name, avatar, geo}) {
+    await mongoose.connect(mongo_url, {
+      useMongoClient: true
+    });
+    let new_location = new LOCATION({
+      id: id,
+      name: name,
+      avatar: avatar,
+      geo: JSON.stringify(geo),
+      reviews: []
+    });
+    return new_location.save((err) => {
+      if (err) {
+        return console.log('Unable to add new location', err);
+      }
+    });
+  }
+};
+
+const addReviewField = {
+  type: reviewType,
+  args: {
+    id: {
+      name: 'id',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
+    },
+    location_id: {
+      name: 'location ID',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
+    },
+    user_id: {
+      name: 'user ID',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
+    },
+    text: {
+      name: 'review text contents',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+    },
+    score: {
+      name: 'review score',
+      type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
+    }
+  },
+  resolve: async function(_, {id, location_id, user_id, text, score}) {
+    try {
+      await mongoose.connect(mongo_url, {
+        useMongoClient: true
+      });
+
+      await USER.findOneAndUpdate({
+        'id': user_id
+      },
+      {
+        $push: {
+          "reviews": id
+        }
+      },
+      (err) => {
+        if (err) {
+          return console.log('Could not update location reviews', err);
+        }
+      });
+
+      await LOCATION.findOneAndUpdate({
+        'id': location_id
+      },
+      {
+        $push: {
+          "reviews": id
+        }
+      },
+      (err) => {
+        if (err) {
+          return console.log('Could not update location reviews', err);
+        }
+      });
+
+      let new_review = new REVIEW({
+        'id': id,
+        'user_id': user_id,
+        'location_id': location_id,
+        'score': score,
+        'text': text,
+        'posted': Date()
+      });
+
+      return new_review.save((err) => {
+        if (err) {
+          return console.log('Could not add new review', err);
+        }
+      });
+    }
+    catch (err) {
+      return console.log(err);
+    }
+
+  }
+};
 
 const mutation = new graphql.GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    add_reviewer: {
-      type: userType,
-      args: {
-        id: {
-          name: 'id',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        },
-        name: {
-          name: 'user name',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-        },
-        avatar: {
-          name: 'avatar url',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-        }
-      },
-      resolve: async function(_, {id, name, avatar}) {
-        await mongoose.connect(mongo_url, {
-          useMongoClient: true
-        });
-        let new_user = new USER({
-          id: id,
-          name: name,
-          avatar: avatar,
-          reviews: [],
-          registered: Date()
-        });
-        return new_user.save((err) => {
-          if (err) {
-            return console.log('Unable to add new reviewer ', err);
-          }
-        });
-      }
-    },
-    add_location: {
-      type: locationType,
-      args: {
-        id: {
-          name: 'id',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        },
-        name: {
-          name: 'user name',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-        },
-        avatar: {
-          name: 'avatar url',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-        },
-        geo: {
-          name: 'Geo coordinates',
-          type: new graphql.GraphQLNonNull(geoType),
-        }
-      },
-      resolve: async function(_, {id, name, avatar, geo}) {
-        await mongoose.connect(mongo_url, {
-          useMongoClient: true
-        });
-        let new_location = new LOCATION({
-          id: id,
-          name: name,
-          avatar: avatar,
-          geo: JSON.stringify(geo),
-          reviews: []
-        });
-        return new_location.save((err) => {
-          if (err) {
-            return console.log('Unable to add new location', err);
-          }
-        });
-      }
-    },
-    add_review: {
-      type: reviewType,
-      args: {
-        id: {
-          name: 'id',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        },
-        location_id: {
-          name: 'location ID',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        },
-        user_id: {
-          name: 'user ID',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        },
-        text: {
-          name: 'review text contents',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-        },
-        score: {
-          name: 'review score',
-          type: new graphql.GraphQLNonNull(graphql.GraphQLInt)
-        }
-      },
-      resolve: async function(_, {id, location_id, user_id, text, score}) {
-        try {
-          await mongoose.connect(mongo_url, {
-            useMongoClient: true
-          });
-
-          USER.update({
-            'id': user_id
-          },
-          {
-            $push: {
-              "reviews": id
-            }
-          },
-          (err) => {
-            return console.log('Could not update user reviews ', err);
-          });
-
-          LOCATION.update({
-            'id': location_id
-          },
-          {
-            $push: {
-              "reviews": id
-            }
-          },
-          (err) => {
-            return console.log('Could not update location reviews', err);
-          });
-
-          let new_review = new REVIEW({
-            'id': id,
-            'user_id': user_id,
-            'location_id': location_id,
-            'score': score,
-            'text': text,
-            'posted': Date()
-          });
-
-          return new_review.save((err) => {
-            if (err) {
-              return console.log('Could not add new review', err);
-            }
-          });
-        }
-        catch (err) {
-          return console.log(err);
-        }
-
-      }
-    }
+    add_reviewer: addReviewerField,
+    add_location: addLocationField,
+    add_review: addReviewField
   }
 });
 
