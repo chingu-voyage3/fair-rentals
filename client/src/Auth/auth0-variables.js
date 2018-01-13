d0521e81f20e81cdcc8b79034a1c4357a1c7dd98
RL Brown <rlbrown72@gmail.com>
2018-01-13 13:39:48 -0500

    testing preinstall script idea

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,8 +1,12 @@
+const callbackUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/callback'
+    : 'https://fair-rentals.herokuapp.com/callback';
 
 const AUTH_CONFIG = {
   domain: 'app83259596.auth0.com',
   clientId: 'NzfwRMKCnd24fhh37tuNDMCzwVrM-GQC',
-  callbackUrl: `https://bears-13.herokuapp.com/callback`,
+  callbackUrl,
 };
 
 export default AUTH_CONFIG;
